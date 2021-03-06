ee49c7f4e4049e7c4186478c8c31a2a39a9e0a51
Marcos Mellado <marcos@mellados.com>
2021-03-06 18:41:06 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <header><nav>Navigation bar here</nav></header>
+    <main>
+      <Nuxt />
+    </main>
+    <footer><nav>Footer here</nav></footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">pokelink-website</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  .title {
+    color: red;
+  }
+
+  .links a {
+    color: blue;
+  }
+}
+</style>
